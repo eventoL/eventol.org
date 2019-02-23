ba0dd1f81d87a44490814b3cacc0379e50846a94
FedeG <federicogonzalez761@gmail.com>
2019-02-23 16:08:42 -0300

    Create constants file

---
+++ src/js/constants.js
@@ -0,0 +1,10 @@
+export const CONTRIBUTORS_URL = 'https://api.github.com/repos/eventol/eventol/contributors';
+export const TAGS_URL = 'https://api.github.com/repos/eventol/eventol/tags';
+export const REPO_URL = 'https://api.github.com/repos/eventol/eventol';
+
+export const CHANGELOG = [
+  '08:30 - 09:00 Breakfast in CR 2A',
+  '09:00 - 10:30 Live sessions in CR 3',
+  '10:30 - 10:45 Break',
+  '10:45 - 12:00 Live sessions in CR 3',
+];
