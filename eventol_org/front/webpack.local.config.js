20f311c2f283ea0f91126eaa679ae01c5ddf10ac
FedeG <federicogonzalez761@gmail.com>
2020-05-22 05:14:28 -0300

    Add front files (webpack, jest, babel configurations)

---
+++ eventol_org/front/webpack.local.config.js
@@ -0,0 +1,53 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const webpack = require('webpack');
+const BundleTracker = require('webpack-bundle-tracker');
+const config = require('./webpack.base.config.js');
+const localSettings = require('./webpack.local-settings.js');
+
+const {ip} = localSettings;
+const port = 3000;
+
+config.devServer = {
+  historyApiFallback: true,
+  noInfo: true,
+  overlay: true,
+  port,
+  host: ip,
+  disableHostCheck: true,
+  public: `${ip}:${port}`,
+  publicPath: `http://${ip}:${port}`,
+};
+
+const addDevVendors = module => [
+  `webpack-dev-server/client?http://${ip}:${port}`,
+  'webpack/hot/only-dev-server',
+  module,
+];
+
+config.devtool = '#eval-source-map';
+
+// Use webpack dev server
+config.entry = {
+  Home: addDevVendors('./src/views/Home'),
+  base: addDevVendors('./src/libs/base'),
+  vendors: ['react', '@babel/polyfill'],
+};
+
+config.mode = 'development';
+
+// override django's STATIC_URL for webpack bundles
+config.output.publicPath = `http://${ip}:${port}/assets/bundles/`;
+
+// Add HotModuleReplacementPlugin and BundleTracker plugins
+config.plugins = config.plugins.concat([
+  new webpack.HotModuleReplacementPlugin(),
+  new webpack.NoEmitOnErrorsPlugin(),
+  new BundleTracker({filename: './webpack-stats-local.json'}),
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify('development'),
+    },
+  }),
+]);
+
+module.exports = config;
