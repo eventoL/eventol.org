7533b9d7eed9e7320947ec3a3a04496cab9f2620
FedeG <federicogonzalez761@gmail.com>
2020-05-22 05:19:02 -0300

    Fix example component: remove unused code

--- eventol_org/front/src/views/Home.jsx
+++ eventol_org/front/src/views/Home.jsx
@@ -1,23 +1,17 @@
 import React from 'react';
 import {render} from 'react-dom';
-import {Provider} from 'react-redux';
 
-import getStore from '../store';
 import Home from '../containers/Home';
 
 import './index.scss';
 
-const store = getStore();
-
 window.render_components = properties => {
   window.params = {...properties};
   render(
-    <Provider store={store}>
-      <Home
-        handleOnChangeLanguage={properties.onChangeLanguage}
-        languages={properties.languages}
-      />
-    </Provider>,
+    <Home
+      handleOnChangeLanguage={properties.onChangeLanguage}
+      languages={properties.languages}
+    />,
     document.getElementById('root')
   );
 };
