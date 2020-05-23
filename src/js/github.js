74ed16324695e941a52f8b8739708c9813587025
FedeG <federicogonzalez761@gmail.com>
2020-05-23 17:28:36 -0300

    Remove prev files and move code to front folder

--- src/js/github.js
+++
@@ -1,7 +0,0 @@
-export const get = (url, field) => fetch(url)
-  .then(res => res.json())
-  .then(json => json[field]);
-
-export const getLength = url => fetch(url)
-  .then(res => res.json())
-  .then(json => json.length);
